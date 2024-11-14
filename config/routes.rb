bf53a7aac3ef3417d2050298344a72f9123c2565
Tim Riley <tim@riley.id.au>
2024-11-14 17:04:11 -0600

    Add a root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 
 module CowboyHat
   class Routes < Hanami::Routes
-    # Add your routes here. See https://guides.hanamirb.org/routing/overview/ for details.
+    root { "Here's the app, Richard!" }
   end
 end
