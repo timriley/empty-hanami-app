06e66099bc81ea200f113dcb092fa4a486d4ccf9
Tim Riley <tim@riley.id.au>
2024-11-14 16:56:12 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CowboyHat
---
+++ app/db/relation.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "hanami/db/relation"
+
+module CowboyHat
+  module DB
+    class Relation < Hanami::DB::Relation
+    end
+  end
+end
